6be7243565e0260f986c817852d15a3173625cd4
Harry Turnbull <hturnbull@live.co.uk>
2020-03-07 15:01:05 +0000

    added some comments to explain directory

--- directory.rb
+++ directory.rb
@@ -1,16 +1,18 @@
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall, we have "
+# list of students
+puts 'The students of Villains Academy'
+puts '-------------'
+puts 'Dr. Hannibal Lecter'
+puts 'Darth Vader'
+puts 'Nurse Ratched'
+puts 'Michael Corleone'
+puts 'Alex DeLarge'
+puts 'The Wicked Witch of the West'
+puts 'Terminator'
+puts 'Freddy Krueger'
+puts 'The Joker'
+puts 'Joffrey Baratheon'
+puts 'Norman Bates'
+# total number of students on one line using print
+print 'Overall, we have '
 print 11
-puts " great students"
+puts ' great students'
