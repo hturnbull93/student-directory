aca9ad7b91cc314fe6c570707b1facefd590017c
Harry Turnbull <hturnbull@live.co.uk>
2020-03-07 15:13:15 +0000

    refactored for each part of list output to a method

--- directory.rb
+++ directory.rb
@@ -13,12 +13,21 @@ students = [
   'Norman Bates'
 ]
 
-# introduce student list
-puts 'The students of Villains Academy'
-puts '-------------'
-# loop through student array putsing each
-students.each do |student|
-  puts student
+def print_header
+  puts 'The students of Villains Academy'
+  puts '-------------'
 end
-# total number of students on one line using string interpolation
-puts "Overall, we have #{students.length} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
